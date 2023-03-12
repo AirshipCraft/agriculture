cd7e474fd74e7808ca616afb164ae2d78c5a63de
zeyad rashed <brwnboy2002@gmail.com>
2023-03-12 12:23:11 -0400

    init repo

---
+++ src/main/java/tk/airshipcraft/Agriculture.java
@@ -0,0 +1,17 @@
+package tk.airshipcraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Agriculture extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
